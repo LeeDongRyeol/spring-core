4b249aad053f6930872b4b4b4b6105028dbc23ea
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2022-01-26 01:18:33 +0900

    anonymous

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -27,10 +28,9 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();  // RateDiscountPolicy로 바꿀때 여기만 바꾸면 된다.
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();  // RateDiscountPolicy로 바꿀때 여기만 바꾸면 된다.
     }
 
     // 메서드를 보면 역할들을 다 알 수 있다.
-
-
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -8,6 +8,8 @@ public class MemberServiceImpl implements MemberService{
     //      인터페이스에 의존하는 memberRepository(추상화된) = 구체화된 호출
     // 추상화 구체화 둘다 사용중이므로 좋은 코드가 아니다.
     // 변경 시 문제가 된다. DIP를 위반하고 있는 코드이다.
+    // 지금은 추상화에만 의존하고 있다
+    // AppConfig에서 구체화 코드를 의존하게 했기 때문에 OCP와 DIP를 만족하는 코드이다.
 
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
