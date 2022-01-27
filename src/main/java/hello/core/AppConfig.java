d9180f60a47a6b9c697b4c1589bffee0ebd607db
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2022-01-28 00:47:23 +0900

    스프링 빈 컨테이너 생성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,29 +8,57 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//public class AppConfig {
+//
+//    // Ctrl + E -> 과거에 사용한 히스토리 클래스들이 다 나옴
+//
+//    // 생성자 주입
+//    // Ctrl + Alt + M => Extract Method
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    public MemoryMemberRepository memberRepository() {
+//        return new MemoryMemberRepository(); // DBRepository로 바뀔때 여기만 바꾸면 된다.
+//    }
+//
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+//
+//    public DiscountPolicy discountPolicy() {
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();  // RateDiscountPolicy로 바꿀때 여기만 바꾸면 된다.
+//    }
+//
+//    // 메서드를 보면 역할들을 다 알 수 있다.
+//}
+
+@Configuration  // : ==> 설정정보
 public class AppConfig {
 
-    // Ctrl + E -> 과거에 사용한 히스토리 클래스들이 다 나옴
-
-    // 생성자 주입
-    // Ctrl + Alt + M => Extract Method
+            // Key : memberService , Value : new MemberServiceImpl(memberRepository())
+    @Bean   // @Bean 을 붙여주면 스프링 컨테이너에 등록된다.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository(); // DBRepository로 바뀔때 여기만 바꾸면 된다.
     }
 
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
+        return new OrderServiceImpl(memberRepository()
+                                    , discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();  // RateDiscountPolicy로 바꿀때 여기만 바꾸면 된다.
     }
-
-    // 메서드를 보면 역할들을 다 알 수 있다.
 }
