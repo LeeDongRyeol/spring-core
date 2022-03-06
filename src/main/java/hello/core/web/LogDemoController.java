e1f16b26fd23a6be79048ac5e69961f225982bcb
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2022-03-07 01:34:30 +0900

    빈 스코프

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;    // Request가 들어와야 오류가 안나는데 애초에 리퀘스트가 없으니 오류가 났음
+
+    // @ResponseBody를 사용하면 넘겨온 문자를 그대로 보낼 수 있다.
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
