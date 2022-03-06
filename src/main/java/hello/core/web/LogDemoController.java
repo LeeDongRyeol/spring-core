1a300e208b9643174a15c46f4dcae543f4b318cf
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2022-03-07 02:04:26 +0900

    스코프에서 프록시 적용

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,19 +14,22 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;    // Request가 들어와야 오류가 안나는데 애초에 리퀘스트가 없으니 오류가 났음
+//    private final ObjectProvider<MyLogger> myLoggerProvider;    // Request가 들어와야 오류가 안나는데 애초에 리퀘스트가 없으니 오류가 났음
+    private final MyLogger myLogger;
 
     // @ResponseBody를 사용하면 넘겨온 문자를 그대로 보낼 수 있다.
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
-
 }
